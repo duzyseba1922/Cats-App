e9c2ae21b63034c109ca077468b55a58b2b1d683
Sebastian Niestój <niestoj.sebastian@gmail.com>
2025-06-16 15:35:51 +0200

    Initial configuration

---
+++ Cats App/Cats_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_AppApp.swift
+//  Cats App
+//
+//  Created by Sebastian Niestój on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cats_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
